4eca022e0f9c70b049efecca637b7faf9f89b29a
nomji <navjeetschattha@gmail.com>
2022-10-11 15:25:38 -0700

    stub functions in order api

---
+++ src/utilities/orders-api.js
@@ -0,0 +1,23 @@
+import sendRequest from "./send-request";
+
+const BASE_URL = '/api/orders'
+
+export function fetchCart(){
+
+}
+
+export function itemInCart(){
+
+}
+
+export function itemQtyCart(){
+
+}
+
+export function checkout(){
+
+}
+
+export function getOrderHistory(){
+    
+}
