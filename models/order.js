f08fb25b9bccd47b75f68dfa9ac845f6f2294ef4
nomji <navjeetschattha@gmail.com>
2022-10-11 14:32:53 -0700

    did the orderschema with an embbed schema

--- models/order.js
+++ models/order.js
@@ -2,6 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const itemSchema = require('./itemSchema');
 
+
+
+
+
 const orderSchema = new Schema({
-    
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    lineItems:[lineItemsSchema],
+    paid:{
+        type: Boolen, 
+        default: false
+    }
+},{
+    timestamps:true,
+    toJson: {
+        virtuals : true
+    }
 });
