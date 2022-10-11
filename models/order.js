2fe1fceb818b577f956f83ce8fa14fb97b5b0cc3
nomji <navjeetschattha@gmail.com>
2022-10-11 14:39:02 -0700

    itemListSchema filled out

--- models/order.js
+++ models/order.js
@@ -3,7 +3,14 @@ const Schema = mongoose.Schema;
 const itemSchema = require('./itemSchema');
 
 const lineItemsSchema = new Schema({
-
+    qty: {
+        type: Number,
+        default: 1
+    },
+    item : itemSchema
+},{
+    timestamps: true,
+    toJSON: {virtuals: true}
 });
 
 
