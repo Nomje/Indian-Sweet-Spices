f8bca1844bd9949a4b5bb1f0ba6f83ed9bae2113
nomji <navjeetschattha@gmail.com>
2022-10-11 13:07:43 -0700

    added in name and sortOrder

--- models/category.js
+++ models/category.js
@@ -2,5 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
-    
-})
+    name:{
+        type : String,
+        required : true,
+    },
+    sortOrder: Number
+    },{
+        timestamps: true
+    })
+
+modeule.exports = mongoose.model('Category', categorySchema);
